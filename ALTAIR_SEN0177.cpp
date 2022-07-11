67886125932ad1e8758b536052693ec12da365d6
adithibalaji <106688613+adithibalaji@users.noreply.github.com>
2022-07-11 14:36:23 -0700

    Update ALTAIR_SEN0177.cpp

--- ALTAIR_SEN0177.cpp
+++ ALTAIR_SEN0177.cpp
@@ -1,61 +1,52 @@
-#include "ALTAIR_SEN0177.h"
+# include "ALTAIR_SEN0144.h"
+
+#define VCC 5.0
+#define INT_VALS 1024.0
+#define DENSITY_MULTIPLIER 0.17
+#define DENSITY_OFFSET 0.1
 
 
-//Constructor
-ALTAIR_SEN0177::ALTAIR_SEN0177(){
+
+//Constructor with input indicating sensor output and LED pin locations
+ALTAIR_SEN0144::ALTAIR_SEN0144(int measurePin, int ledPower) {
+  measurePin = measurePin;
+  ledPower = ledPower;
+  voMeasured = calcVoltage = dustDensity = 0;
+}
+
+
+
+//Drive LED and read raw dust value
+void ALTAIR_SEN0144::measureSample(int samplingTime, int deltaTime){
   
-  int PM01Value = 0;
-  int PM2_5ValuE = 0;
-  int PM10Value = 0;
+  pinMode(ledPower, OUTPUT);
+  digitalWrite(ledPower,LOW); // power on the LED
+  delayMicroseconds(samplingTime);
 
+  voMeasured = analogRead(measurePin); // read the dust value
 
-}
-//Get PM1.0 value
-int ALTAIR_SEN0177::transmitPM01(unsigned char *thebuf)
-{
-  int PM01Val;
-  PM01Val=((thebuf[3]<<8) + thebuf[4]); //count PM1.0 value of the air detector module
-  return PM01Val;
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH); // turn the LED off
 }
 
-//Get PM2.5 value
-int ALTAIR_SEN0177::transmitPM2_5(unsigned char *thebuf)
-{
-  int PM2_5Val;
-  PM2_5Val=((thebuf[5]<<8) + thebuf[6]);//count PM2.5 value of the air detector module
-  return PM2_5Val;
-  }
+//Calculate dust density
+void ALTAIR_SEN0144::calcDustDensity(){
+   // 0 - 5V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (VCC / INT_VALS);
 
-//Get PM10 value
-int ALTAIR_SEN0177::transmitPM10(unsigned char *thebuf){
-  int PM10Val;
-  PM10Val=((thebuf[7]<<8) + thebuf[8]);//count PM10 value of the air detector module
-  return PM10Val;
-  }
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = DENSITY_MULTIPLIER * calcVoltage - DENSITY_OFFSET;
+}
 
-//Run sensor and print dust values to serial monitor
-void ALTAIR_SEN0177::printDustVal(){
 
-    unsigned char buf[LENG];
-    if(Serial.find(0x42)){    //start to read when detect 0x42
-    Serial.readBytes(buf,LENG);
+//Output dust value
+void ALTAIR_SEN0144::printDustVal(int samplingTime, int deltaTime){
+  measureSample(samplingTime,deltaTime);
+  calcDustDensity();
+  Serial.print("Dust density: ");
+  Serial.print(dustDensity);
+  Serial.println("  mg/m3");
 
-    if(buf[0] == 0x4d){
-        PM01Value=transmitPM01(buf); //count PM1.0 value of the air detector module
-        PM2_5Value=transmitPM2_5(buf);//count PM2.5 value of the air detector module
-        PM10Value=transmitPM10(buf); //count PM10 value of the air detector module
-    }
-   }
-      Serial.print("PM1.0: ");
-      Serial.print(PM01Value);
-      Serial.println("  ug/m3");
-
-      Serial.print("PM2.5: ");
-      Serial.print(PM2_5Value);
-      Serial.println("  ug/m3");
-
-      Serial.print("PM1 0: ");
-      Serial.print(PM10Value);
-      Serial.println("  ug/m3");
-      Serial.println();
 }
