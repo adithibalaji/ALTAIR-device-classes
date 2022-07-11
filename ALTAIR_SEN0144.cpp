83b8fe040213e236188d8ea3cd8c94eaa1aeae68
adithibalaji <106688613+adithibalaji@users.noreply.github.com>
2022-07-11 14:35:32 -0700

    Update ALTAIR_SEN0144.cpp

--- ALTAIR_SEN0144.cpp
+++ ALTAIR_SEN0144.cpp
@@ -1,18 +1,19 @@
 # include "ALTAIR_SEN0144.h"
 
-#define VCC 5.0;
-#define INT_VALS 1024.0;
-#define DENSITY_MULTIPLIER 0.17;
-#define DENSITY_OFFSET 0.1;
+#define VCC 5.0
+#define INT_VALS 1024.0
+#define DENSITY_MULTIPLIER 0.17
+#define DENSITY_OFFSET 0.1
 
 
 
 //Constructor with input indicating sensor output and LED pin locations
-ALTAIR_SEN0144::ALTAIR_SEN1044(int measurePin, int ledPower) {
-  _measurePin = measurePin;
-  _ledPower = ledPower;
+ALTAIR_SEN0144::ALTAIR_SEN0144(int measurePin, int ledPower) {
+  measurePin = measurePin;
+  ledPower = ledPower;
   voMeasured = calcVoltage = dustDensity = 0;
 }
+
 
 
 //Drive LED and read raw dust value
