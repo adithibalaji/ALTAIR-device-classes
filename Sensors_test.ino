c84e461aa30c6f83f523f14a643f65df59a075f4
adithibalaji <106688613+adithibalaji@users.noreply.github.com>
2022-07-19 14:21:20 -0700

    Update Sensors_test.ino

--- Sensors_test.ino
+++ Sensors_test.ino
@@ -1,11 +1,15 @@
 
+
 #include <ALTAIR_SEN0133.h>
 #include <ALTAIR_SEN0144.h>
 #include <ALTAIR_SEN0177.h>
+#include <ALTAIR_HCSR04.h>
+
 
 //ALTAIR_SEN0133 sen0133(0,1000);
-//ALTAIR_SEN0144 sen0144(0,2);
-ALTAIR_SEN0177 sen0177;
+//ALTAIR_SEN0144 sen0144(0,1000);
+//ALTAIR_SEN0177 sen0177;
+  ALTAIR_HCSR04 hcsr04; 
 
 void setup() {
   //Initialize serial baud rate
@@ -13,8 +17,9 @@ void setup() {
 }
 
 void loop() {
-  //Test sensor output here
+//Test sensor output here
 //  sen0133.printGasVal();
-//  sen0144.measureSample(280,40);
-  sen0177.printDustVal();
+//sen0144.printDustVal();
+// sen0177.printDustVal();
+hcsr04.printDistance();
 }
