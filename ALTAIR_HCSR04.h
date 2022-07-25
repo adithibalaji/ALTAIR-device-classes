79db756b419aa6b6d70d78cbbcbb68e41f3f0465
jalbert10NJ <jalbert@uvic.ca>
2022-07-24 21:54:18 -0700

    Update ALTAIR_HCSR04.h

--- ALTAIR_HCSR04.h
+++ ALTAIR_HCSR04.h
@@ -1,23 +1,27 @@
 #ifndef ALTAIR_HCSR04_h
 #define ALTAIR_HCSR04_h
 
+#define MAX_HCSR04_DISTANCE        350       // cm
+#define SPEED_OF_SOUND               0.0343  // cm per us
+#define HCSR04_ECHO_PIN              3
+#define HCSR04_TRIG_PIN              2
+#define HCSR04_MIN_DELAY_BTW_PINGS  50       // ms
+
 #include "Arduino.h"
 #include <Newping.h>
 
 class ALTAIR_HCSR04{
   public:
 
-    ALTAIR_HCSR04(      )               ;
+    ALTAIR_HCSR04(      ) ;
 
-    float duration(     ) { return _dura; }
-    float distance(     ) { return _dist; }
+    float getdistance(  ) ;
 
-    void  printDistance()               ;
+    void  printDistance() ;
   
   private:
   
-    float _dura                         ;
-    float _dist                         ;
+    NewPing _sonar        ;
 
 };
 
