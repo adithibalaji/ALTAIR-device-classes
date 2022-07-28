cffda5007e4e7a445e2413e56c29d35b11cf1766
jalbert10NJ <jalbert@uvic.ca>
2022-07-28 14:32:32 -0700

    Update ALTAIR_SEN0133.h

--- ALTAIR_SEN0133.h
+++ ALTAIR_SEN0133.h
@@ -1,3 +1,22 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_SEN0133.h
+    @author   Adithi Balaji (adithibalaji@uvic.ca)
+    @license  GPL
+    
+    This is the class for the ALTAIR SEN0133 hydrogen gas sensor, which is
+    not by default located onboard ALTAIR (yet), but perhaps will be  
+    mounted onboard ALTAIR (in at least some flight configurations) 
+    sometime in the future.
+    
+    Adithi Balaji  adithibalaji@uvic.ca  began in June 2022
+    Justin Albert  jalbert@uvic.ca       updated on 28 July 2022
+    
+    @section  HISTORY
+    v1.0  - First version
+*/
+/**************************************************************************/
+
 #ifndef   ALTAIR_SEN0133_h
 #define   ALTAIR_SEN0133_h
 
@@ -6,13 +25,14 @@
 class ALTAIR_SEN0133 {
   
   public:
+    ALTAIR_SEN0133(  );
+
+    int  getGasVal(  );
+    void printGasVal();
   
-  ALTAIR_SEN0133();
+  private:
+    int _hydrogenVal  ;
 
-  int hydrogenVal;
-
-
-  void printGasVal();
 };
 
 #endif
