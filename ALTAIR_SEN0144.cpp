aa5ca3699fbaf9ed6598222abc6c41af5d44662a
jalbert10NJ <jalbert@uvic.ca>
2022-07-28 16:21:11 -0700

    Update ALTAIR_SEN0144.cpp

--- ALTAIR_SEN0144.cpp
+++ ALTAIR_SEN0144.cpp
@@ -1,4 +1,25 @@
-# include "ALTAIR_SEN0144.h"
+/**************************************************************************/
+/*!
+    @file     ALTAIR_SEN0144.cpp
+    @author   Adithi Balaji (adithibalaji@uvic.ca)
+    @license  GPL
+    
+    This is the class for the ALTAIR SEN0144 dust sensor, which contains
+    the Sharp Optical Dust Sensor GP2Y1010AU0F.  This sensor is not by
+    default located onboard ALTAIR (yet), but perhaps will be mounted 
+    onboard ALTAIR (in at least some flight configurations) sometime in
+    the future.  Since the SEN0144 is a DFRobot product, this code is 
+    adapted from sample code on the DFRobot website.
+    
+    Adithi Balaji  adithibalaji@uvic.ca  began in June 2022
+    Justin Albert  jalbert@uvic.ca       updated on 28 July 2022
+    
+    @section  HISTORY
+    v1.0  - First version
+*/
+/**************************************************************************/
+
+#include "ALTAIR_SEN0144.h"
 
 #define VCC 5.0
 #define INT_VALS 1024.0
@@ -9,38 +30,39 @@
 
 
 //Constructor with input indicating sensor output and LED pin locations
-ALTAIR_SEN0144::ALTAIR_SEN0144() {
-
-  voMeasured = calcVoltage = dustDensity = 0;
+ALTAIR_SEN0144::ALTAIR_SEN0144( ) : 
+  _voMeasured(  0.              ) ,
+  _calcVoltage( 0.              ) ,
+  _dustDensity( 0.              )
+{
 }
 
-
-
 //Drive LED and read raw dust value
-void ALTAIR_SEN0144::measureSample(){
-
-  voMeasured = analogRead(measurePin); // read the dust value
-  delay(sampleDelay);
+void ALTAIR_SEN0144::measureSample(    ) {
+  _voMeasured = analogRead( measurePin ) ; // read the dust value
 }
 
 //Calculate dust density
-void ALTAIR_SEN0144::calcDustDensity(){
+void ALTAIR_SEN0144::calcDustDensity(  ) {
    // 0 - 5V mapped to 0 - 1023 integer values
   // recover voltage
-  calcVoltage = voMeasured * (VCC / INT_VALS);
+  _calcVoltage = _voMeasured * (VCC / INT_VALS);
 
   // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
   // Chris Nafis (c) 2012
-  dustDensity = DENSITY_MULTIPLIER * calcVoltage - DENSITY_OFFSET;
+  _dustDensity = DENSITY_MULTIPLIER * _calcVoltage - DENSITY_OFFSET;
 }
 
 
 //Output dust value
-void ALTAIR_SEN0144::printDustVal(){
-  measureSample();
-  calcDustDensity();
-  Serial.print("Dust density: ");
-  Serial.print(dustDensity);
-  Serial.println("  mg/m3");
+void ALTAIR_SEN0144::printDustVal(                  ) {
+  measureSample(                                    ) ;
+  calcDustDensity(                                  ) ;
+  Serial.print("Dust density: "                     ) ;
+  Serial.print(_dustDensity                         ) ;
+  Serial.println("  mg/m3"                          ) ;
+}
 
+void ALTAIR_SEN0144::delayBtwReads( int delay_in_ms ) {
+  delay(         delay_in_ms                        ) ;  
 }
