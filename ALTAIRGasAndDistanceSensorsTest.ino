32cb5560840834f2c08414454ad6fe9210ce563c
jalbert10NJ <jalbert@uvic.ca>
2022-07-28 16:29:01 -0700

    Update ALTAIRGasAndDistanceSensorsTest.ino

--- ALTAIRGasAndDistanceSensorsTest.ino
+++ ALTAIRGasAndDistanceSensorsTest.ino
@@ -6,20 +6,21 @@
 #include <ALTAIR_HCSR04.h>
 
 
-//ALTAIR_SEN0133 sen0133;
-//ALTAIR_SEN0144 sen0144;
-//ALTAIR_SEN0177 sen0177;
-  ALTAIR_HCSR04 hcsr04; 
+// ALTAIR_SEN0133 sen0133;
+// ALTAIR_SEN0144 sen0144;
+// ALTAIR_SEN0177 sen0177;
+ALTAIR_HCSR04 hcsr04; 
 
 void setup() {
-  //Initialize serial baud rate
+// Initialize serial baud rate
   Serial.begin(38400);
 }
 
 void loop() {
-//Test sensor output here
-//sen0133.delayBtwReads; sen0133.printHydrogenGasVal();
-//sen0144.delayBtwReads; sen0144.printDustVal();
-//sen0177.delayBtwReads; sen0177.printDustVal();
-hcsr04.delayBtwPings(); hcsr04.printDistance();
+// Test sensor output here
+
+//  sen0133.delayBtwReads(); sen0133.printHydrogenGasVal();
+//  sen0144.delayBtwReads(); sen0144.printDustVal();
+//  sen0177.delayBtwReads(); sen0177.printDustVal();
+  hcsr04.delayBtwPings();  hcsr04.printDistance();
 }
