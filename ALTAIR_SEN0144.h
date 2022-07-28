9e76026fdd422e392bde406e57b5afa6c8c1b3ef
jalbert10NJ <jalbert@uvic.ca>
2022-07-28 16:11:57 -0700

    Update ALTAIR_SEN0144.h

--- ALTAIR_SEN0144.h
+++ ALTAIR_SEN0144.h
@@ -1,29 +1,45 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_SEN0144.h
+    @author   Adithi Balaji (adithibalaji@uvic.ca)
+    @license  GPL
+    
+    This is the class for the ALTAIR SEN0144 dust sensor, which contains
+    the Sharp Optical Dust Sensor GP2Y1010AU0F.  This sensor is not by
+    default located onboard ALTAIR (yet), but perhaps will be mounted 
+    onboard ALTAIR (in at least some flight configurations) sometime in
+    the future.  Since the SEN0144 is a DFRobot product, this code is 
+    adapted from sample code on the DFRobot website.
+    
+    Adithi Balaji  adithibalaji@uvic.ca  began in June 2022
+    Justin Albert  jalbert@uvic.ca       updated on 28 July 2022
+    
+    @section  HISTORY
+    v1.0  - First version
+*/
+/**************************************************************************/
+
 #ifndef   ALTAIR_SEN0144_h
 #define   ALTAIR_SEN0144_h
 
 #include "Arduino.h"
 
-/*Class for Sharp Optical Dust Sensor GP2Y1010AU0F, adapted from sample code on DFRobot website
- * 
- */
 class ALTAIR_SEN0144 {
   
   public:
   
-  ALTAIR_SEN0144();
+    ALTAIR_SEN0144(         ) ;
 
+    void   measureSample(   ) ;
+    void   calcDustDensity( ) ;
+    void   printDustVal(    ) ;
   
-
-  float voMeasured;
-  float calcVoltage;
-  float dustDensity;
-
-
-  void measureSample();
-  void calcDustDensity();
-  void printDustVal();
-\
-    
+  private:
+  
+    float _voMeasured         ;
+    float _calcVoltage        ;
+    float _dustDensity        ;
+  
 };
 
 #endif
