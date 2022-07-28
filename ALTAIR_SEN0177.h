8dbbced1d577660bfaece7f60b96e9af341ac76e
jalbert10NJ <jalbert@uvic.ca>
2022-07-28 16:43:40 -0700

    Update ALTAIR_SEN0177.h

--- ALTAIR_SEN0177.h
+++ ALTAIR_SEN0177.h
@@ -1,28 +1,49 @@
+
+/**************************************************************************/
+/*!
+    @file     ALTAIR_SEN0177.h
+    @author   Adithi Balaji (adithibalaji@uvic.ca)
+    @license  GPL
+    
+    This is the class for the ALTAIR SEN0177 dust sensor.  This sensor is 
+    not by default located onboard ALTAIR (yet), but perhaps will be 
+    mounted onboard ALTAIR (in at least some flight configurations)
+    sometime in the future.  Since the SEN0177 is a DFRobot product, this
+    code is adapted from sample code on the DFRobot website.
+    
+    Adithi Balaji  adithibalaji@uvic.ca  began in June 2022
+    Justin Albert  jalbert@uvic.ca       updated on 28 July 2022
+    
+    @section  HISTORY
+    v1.0  - First version
+*/
+/**************************************************************************/
+
 #ifndef   ALTAIR_SEN0177_h
 #define   ALTAIR_SEN0177_h
 
-#include "Arduino.h"
-#define LENG 31   //0x42 + 31 bytes equal to 32 bytes
-;
+#define   LENG 31   //0x42 + 31 bytes equal to 32 bytes
 
-/*
- * Class for DFrobot SEN 0177 Dust sensor, adapted from wiki code on website
- */
+#include "Arduino.h"
+
 class ALTAIR_SEN0177 {
   
   public:
   
-  ALTAIR_SEN0177();
+    ALTAIR_SEN0177(                           ) ;
 
-  int PM01Value=0;          
-  int PM2_5Value=0;         
-  int PM10Value=0;   
 
-  int transmitPM01(unsigned char *thebuf);
-  int transmitPM2_5(unsigned char *thebuf);
-  int transmitPM10(unsigned char *thebuf);
-  void printDustVal();
-    
+    int  transmitPM01(  unsigned char *thebuf ) ;
+    int  transmitPM2_5( unsigned char *thebuf ) ;
+    int  transmitPM10(  unsigned char *thebuf ) ;
+    void printDustVal(                        ) ;
+
+  private:
+  
+    int _PM01Value                              ;          
+    int _PM2_5Value                             ;         
+    int _PM10Value                              ;     
+  
 };
 
 #endif
