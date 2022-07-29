2515d9934fc1484cdc2c515088abebb900809f0e
jalbert10NJ <jalbert@uvic.ca>
2022-07-29 16:05:17 -0700

    Update ALTAIR_SEN0144.cpp

--- ALTAIR_SEN0144.cpp
+++ ALTAIR_SEN0144.cpp
@@ -21,36 +21,39 @@
 
 #include "ALTAIR_SEN0144.h"
 
-#define VCC 5.0
-#define INT_VALS 1024.0
-#define DENSITY_MULTIPLIER 0.17
-#define DENSITY_OFFSET 0.1
-#define measurePin 0
-#define sampleDelay 1000
 
-
-//Constructor with input indicating sensor output and LED pin locations
-ALTAIR_SEN0144::ALTAIR_SEN0144( ) : 
-  _voMeasured(  0.              ) ,
-  _calcVoltage( 0.              ) ,
-  _dustDensity( 0.              )
+// Default constructor
+ALTAIR_SEN0144::ALTAIR_SEN0144(                 ) :
+  _readoutPin(          SEN0144_NOM_READOUT_PIN ) ,
+  _voMeasured(                               0. ) ,
+  _calcVoltage(                              0. ) ,
+  _dustDensity(                              0. )
 {
 }
 
-//Drive LED and read raw dust value
-void ALTAIR_SEN0144::measureSample(    ) {
-  _voMeasured = analogRead( measurePin ) ; // read the dust value
+// Constructor with input indicating sensor output pin location
+ALTAIR_SEN0144::ALTAIR_SEN0144( byte readoutPin ) :
+  _readoutPin(                       readoutPin ) ,
+  _voMeasured(                               0. ) ,
+  _calcVoltage(                              0. ) ,
+  _dustDensity(                              0. )
+{
 }
 
-//Calculate dust density
-void ALTAIR_SEN0144::calcDustDensity(  ) {
-   // 0 - 5V mapped to 0 - 1023 integer values
+// Drive LED and read raw dust value
+void ALTAIR_SEN0144::measureSample(             ) {
+  _voMeasured = analogRead(         _readoutPin ) ; // read the dust value
+}
+
+// Calculate dust density
+void ALTAIR_SEN0144::calcDustDensity(           ) {
+  // 0 - 5V mapped to 0 - 1023 integer values
   // recover voltage
-  _calcVoltage = _voMeasured * (VCC / INT_VALS);
+  _calcVoltage = _voMeasured * ( VCC / INT_VALS ) ;
 
   // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
   // Chris Nafis (c) 2012
-  _dustDensity = DENSITY_MULTIPLIER * _calcVoltage - DENSITY_OFFSET;
+  _dustDensity = DENSITY_MULTIPLIER * _calcVoltage - DENSITY_OFFSET ;
 }
 
 
